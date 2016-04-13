63a48ac6b5e843346deb90507d8af1197b3a45e3
FredeH <frede.hansen09@gmail.com>
2016-04-14 01:02:40 +0200

    knapper stuff

--- app/src/main/java/com/fsc/gardiner/Gardiner_main.java
+++ app/src/main/java/com/fsc/gardiner/Gardiner_main.java
@@ -1,8 +1,14 @@
 package com.fsc.gardiner;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -12,8 +18,17 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class Gardiner_main extends FragmentActivity implements ActionBar.TabListener {
 
@@ -21,7 +36,18 @@ public class Gardiner_main extends FragmentActivity implements ActionBar.TabList
     AppSectionsPagerAdapter mAppSectionsPagerAdapter;
 
     ViewPager mViewPager;
+    private static Switch BTswitch;
+    private static Button BTconnect;
+    private static Button rulop;
+    private static Button rulned;
+    private static ListView devicelist;
+    private static Set<BluetoothDevice>pairedDevices;
+    private static Context context;
+    private static ArrayAdapter<String> BTArrayAdapter;
 
+
+
+    public static BluetoothAdapter myBluetooth;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +55,11 @@ public class Gardiner_main extends FragmentActivity implements ActionBar.TabList
 
         mAppSectionsPagerAdapter = new AppSectionsPagerAdapter(getSupportFragmentManager());
 
-        final ActionBar actionBar = getActionBar();
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();
+
+        Gardiner_main.context = getApplicationContext();
+
+        //final ActionBar actionBar = getActionBar();
 
 
         mViewPager = (ViewPager) findViewById(R.id.pager);
@@ -41,11 +71,11 @@ public class Gardiner_main extends FragmentActivity implements ActionBar.TabList
                 // We can also use ActionBar.Tab#select() to do this if we have a reference to the
                 // Tab.
                 //actionBar.setSelectedNavigationItem(position);
-                invalidateOptionsMenu();
+                //invalidateOptionsMenu();
             }
         });
 
-
+        /*
         for (int i = 0; i < mAppSectionsPagerAdapter.getCount(); i++) {
             // Create a tab with text corresponding to the page title defined by the adapter.
             // Also specify this Activity object, which implements the TabListener interface, as the
@@ -56,7 +86,7 @@ public class Gardiner_main extends FragmentActivity implements ActionBar.TabList
                                 .setText(mAppSectionsPagerAdapter.getPageTitle(i))
                                 .setTabListener(this));
             }
-        }
+        } */
     }
 
     @Override
@@ -74,7 +104,6 @@ public class Gardiner_main extends FragmentActivity implements ActionBar.TabList
     }
 
 
-
     public static class AppSectionsPagerAdapter extends FragmentPagerAdapter {
 
         public AppSectionsPagerAdapter(FragmentManager fm) {
@@ -89,14 +118,11 @@ public class Gardiner_main extends FragmentActivity implements ActionBar.TabList
                     // a launchpad into the other demonstrations in this example application.
                     return new ForsideFragment();
 
-                default:
-                    // The other sections of the app are dummy placeholders.
-                    Fragment fragment = new DummySectionFragment();
-                 //   Bundle args = new Bundle();
-                 //   args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, i + 1);
-                 //   fragment.setArguments(args);
-                    return fragment;
+                case 1:
+                    //Vis BluetoothFragment efter swipe
+                    return new BluetoothFragment();
             }
+            return null;
         }
 
         @Override
@@ -106,28 +132,121 @@ public class Gardiner_main extends FragmentActivity implements ActionBar.TabList
 
     }
 
+
     public static class ForsideFragment extends Fragment {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.content_gardiner_main, container, false);
-
+            View rootView = inflater.inflate(R.layout.content_gardiner_main, container, false);
 
             return rootView;
+
+
         }
+
+        public void onViewCreated(final View rootView, Bundle savedInstanceState){
+
+            rulop = (Button)rootView.findViewById(R.id.button);
+            rulop.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+            rulned = (Button)rootView.findViewById(R.id.button2);
+        }
+
     }
 
-    public static class DummySectionFragment extends Fragment {
+    public static class BluetoothFragment extends Fragment {
 
         //public static final String ARG_SECTION_NUMBER = "section_number";
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_bluetooth_connecter, container, false);
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.activity_bluetooth_connecter, container, false);
+
+            devicelist = (ListView)rootView.findViewById(R.id.listView);
+
+            BTArrayAdapter = new ArrayAdapter<>(getAppContext(),android.R.layout.simple_list_item_1);
+            devicelist.setAdapter(BTArrayAdapter);
+            // devicelist.setOnItemClickListener(myDeviceClickListener);
+
             return rootView;
+
         }
+
+        /*
+        private AdapterView.OnItemClickListener myDeviceClickListener = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
+                // Cancel discovery because it's costly and we're about to connect
+                myBluetooth.cancelDiscovery();
+                // Get the device MAC address, which is the last 17 chars in the View
+                String info = ((TextView) v).getText().toString();
+                String address = info.substring(info.length() - 17);
+                // Create the result Intent and include the MAC address
+                Intent intent = new Intent();
+                intent.putExtra(EXTRA_DEVICE_ADDRESS, address);
+                // Set result and finish this Activity
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        };
+        */
+
+        public static Context getAppContext() {
+            return Gardiner_main.context;
+        }
+
+        public void list(View rootView){
+            pairedDevices = myBluetooth.getBondedDevices();
+
+            for(BluetoothDevice device : pairedDevices)
+                BTArrayAdapter.add(device.getName()+ "\n" + device.getAddress());
+
+            Toast.makeText(getAppContext(),"Vis sammenkoblet enheder", Toast.LENGTH_LONG).show();
+        }
+
+        /*
+        final BroadcastReceiver bReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action intent.getAction();
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    BTArrayAdapter.add(device.getName()+ "\n" + device.getAddress());
+                    BTArrayAdapter.notifyDataSetChanged();
+                }
+            }
+        };
+        */
+
+        public void onViewCreated(final View rootView, Bundle savedInstanceState){
+
+            BTconnect = (Button)rootView.findViewById(R.id.button3);
+            BTconnect.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                list(v);
+                }
+            });
+
+
+            BTswitch = (Switch)rootView.findViewById(R.id.switch1);
+            BTswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton rootView, boolean isChecked) {
+                    if (isChecked) {
+                        myBluetooth.enable();
+                    } else {
+                        myBluetooth.disable();
+                    }
+                }
+            });
+        }
+
+
     }
 }
-
